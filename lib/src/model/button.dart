4953a930b65b67accf37dc044bc04a17f4e9f0a4
Alberto Gabriel Guaman Garcia <80141815+GbrielGarcia@users.noreply.github.com>
2026-05-24 23:12:19 -0500

    fix: actualizar font_awesome_flutter a v11 por IconData final
    Flutter 3.44+ marca IconData como final; la v10 de font_awesome_flutter dejaba de compilar en web. Se migra a 11.0.0 y ButtonModel usa FaIconData.

--- lib/src/model/button.dart
+++ lib/src/model/button.dart
@@ -4,7 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class ButtonModel {
   final String name;
   final String url;
-  final IconData icon;
+  final FaIconData icon;
 
   ButtonModel({required this.name, required this.url, required this.icon});
 }
